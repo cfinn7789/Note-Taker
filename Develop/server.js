f45224c049a7f211a9c039c2cad6634701b047bb
cfinn7789 <cfinn7789@gmail.com>
2023-08-01 15:55:22 -0400

    added routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,22 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join('/public/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join('/public/notes.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
