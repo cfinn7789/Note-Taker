495ac35447846e7948b059c9735a1a3384f6237e
cfinn7789 <cfinn7789@gmail.com>
2023-08-02 15:21:09 -0400

    updated code in notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,26 +1,41 @@
 const notes = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
-notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
+const filePath = path.join(__dirname, 'db', 'db.json');
 
-notes.post('/', (req, res) => {
-    console.log(req.body);
+const readNotes = () => {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
+
+notes.get('/api/notes', (req, res) => {
+  const currentNotes = readNotes();
+  res.json(currentNotes);
+})
+
+notes.post('/api/notes', (req, res) => {
+  const {title, text} = req.body;
   
-    const { title, text } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        note_id: uuidv4(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(`Note succesfully added`);
-    } else {
-      res.error('Error in adding note');
+  if (req.body) {
+    const newNote = {
+      title,
+      text
     }
-  });
+    readAndAppend(newNote, '/db/db.json');
+    res.json('Note added succesfully')
+  } else {
+    res.error('Error in adding note')
+  }
+});
+
+notes.delete('/api/notes/:id', (req, res) => {
+  
+});
+
 
   module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
   });
